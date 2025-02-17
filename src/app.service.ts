5964f36544981af4671ebf48289f8202fa5de48e
tnchnkit@hotmail.com <kelvingssb@gmail.com>
2025-02-17 23:11:56 +0900

    set up packages and testing

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
+
   getHello(): string {
-    return 'Hello World!';
+    // Accessing a sample environment variable
+    const dbHost = this.configService.get<string>('DB_HOST');
+    return `Database Host: ${dbHost}`;
   }
 }
